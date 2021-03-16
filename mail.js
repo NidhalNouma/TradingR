c4f20bf6b2f1590d64041907759283320a4cd9e3
Nidhal <nidhal.nouma.0@gmail.com>
2021-03-16 15:01:58 +0100

    adding logo.png

--- mail.js
+++ mail.js
@@ -1,6 +1,15 @@
-const nodemailer = require("nodemailer");
+// const nodemailer = require("nodemailer");
 const ejs = require("ejs");
+const AWS = require("aws-sdk");
 require("dotenv").config();
+
+AWS.config.update({ region: "eu-west-2" });
+const ses = new AWS.SES({ apiVersion: "2010-12-01" });
+
+// const ses = new aws.SES({
+//   apiVersion: "2010-12-01",
+//   region: "eu-west-2",
+// });
 
 // const transporter = nodemailer.createTransport({
 //   service: "gmail",
@@ -13,21 +22,25 @@ require("dotenv").config();
 //     refreshToken: process.env.OAUTH_REFRESH_TOKEN,
 //   },
 // });
+// var transporter = nodemailer.createTransport({
+//   host: "smtp.mailtrap.io",
+//   port: 2525,
+//   auth: {
+//     user: "cdd15777442946",
+//     pass: "ba153f7551da9f",
+//   },
+// });
 
-var transporter = nodemailer.createTransport({
-  host: "smtp.mailtrap.io",
-  port: 2525,
-  auth: {
-    user: "cdd15777442946",
-    pass: "ba153f7551da9f",
-  },
-});
+// let transporter = nodemailer.createTransport({
+//   SES: { ses, aws },
+// });
 
 module.exports.sendMail = async function (email, type, data) {
   let r = { res: null, err: null };
   try {
     const mail = await mailOptions(email, type, data);
-    r.res = await transporter.sendMail(mail);
+    // r.res = await transporter.sendMail(mail);
+    r.res = await ses.sendEmail(mail).promise();
     console.log("Email sent: TO " + email);
   } catch (e) {
     console.log("error with sendMail", e);
@@ -50,12 +63,7 @@ const mailOptions = function (email, type, data) {
           console.log(err);
           reject(err);
         } else {
-          resolve({
-            from: process.env.EMAIL_ADDRESS,
-            to: email,
-            subject: "Sending Email",
-            html: str,
-          });
+          resolve(params(email, str, "Reset Password"));
         }
       });
     });
@@ -70,14 +78,35 @@ const mailOptions = function (email, type, data) {
           console.log(err);
           reject(err);
         } else {
-          resolve({
-            from: process.env.EMAIL_ADDRESS,
-            to: email,
-            subject: "Sending Email",
-            html: str,
-          });
+          resolve(params(email, src, "Confirm Email"));
         }
       });
     });
   }
 };
+
+function params(email, html, subject) {
+  return {
+    Destination: {
+      ToAddresses: [email], // Email address/addresses that you want to send your email
+    },
+    // ConfigurationSetName: "<<ConfigurationSetName>>,",
+    Message: {
+      Body: {
+        Html: {
+          Charset: "UTF-8",
+          Data: html,
+        },
+        // Text: {
+        //   Charset: "UTF-8",
+        //   Data: "Hello Charith Sample description time 1517831318946",
+        // },
+      },
+      Subject: {
+        Charset: "UTF-8",
+        Data: subject,
+      },
+    },
+    Source: process.env.EMAIL_ADDRESS,
+  };
+}
